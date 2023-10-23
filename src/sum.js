f50d74e2e11979f7e2f84895f6b961aad0734fe9
hashab1926 <fikrimubarok19@gmail.com>
2023-10-23 07:32:25 +0700

    learn: coverage

--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,11 @@
 export const sum = (first, second) => {
   return first + second;
 }
+
+export const factorial = (number) => {
+  let total = 1;
+  for (let i = 1; i <= number; i++) {
+    total = total * i;
+  }
+  return total;
+}
--- test/exception.test.js
+++ test/exception.test.js
@@ -1,7 +1,13 @@
 import { MyException, callMe } from "../src/exception";
+import { factorial } from "../src/sum";
 
 test("exception", () => {
   expect(() => callMe("saenahow")).toThrow(MyException);
   expect(() => callMe("saenahow")).toThrow();
   expect(() => callMe("saenahow")).toThrow("Ups my exception happens")
+  expect(callMe()).toBe("OK")
+})
+
+test("factorial", () => {
+  expect(factorial(5)).toBe(120);
 })
